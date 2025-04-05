1d17e75e1919ef9b903674e9ae2ef98c99ea8d13
Murshed J. Ahmed <mahmed@marvel.com>
2025-04-05 12:09:26 -0400

    Implement the parseFeed() method and add tests

---
+++ IndexBuilder.java
@@ -0,0 +1,72 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.*;
+
+public class IndexBuilder implements IIndexBuilder {
+    @Override
+    public Map<String, List<String>> parseFeed(List<String> feeds) {
+        Map<String, List<String>> docs = new HashMap<>();
+        for (String rssUrl : feeds) {
+            Document rssDoc;
+            try {
+                // Fetch the RSS document by URL
+                rssDoc = Jsoup.connect(rssUrl).get();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            // Extract all the <link> tags in the RSS feed
+            Elements links = rssDoc.getElementsByTag("link");
+            for (Element link : links) {
+                String htmlUrl = link.text();
+                Document htmlDoc;
+                try {
+                    htmlDoc = Jsoup.connect(htmlUrl).get();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                Elements bodies = htmlDoc.getElementsByTag("body");
+
+                List<String> words = new ArrayList<>();
+                for (Element body : bodies) {
+                    String contents = body.text();
+                    List<String> tokens = StringUtils.tokenize(contents);
+                    words.addAll(tokens);
+                }
+                docs.put(htmlUrl, words);
+            }
+        }
+
+        return docs;
+    }
+
+    @Override
+    public Map<String, Map<String, Double>> buildIndex(Map<String, List<String>> docs) {
+        return Map.of();
+    }
+
+    @Override
+    public Map<?, ?> buildInvertedIndex(Map<String, Map<String, Double>> index) {
+        return Map.of();
+    }
+
+    @Override
+    public Collection<Map.Entry<String, List<String>>> buildHomePage(Map<?, ?> invertedIndex) {
+        return List.of();
+    }
+
+    @Override
+    public Collection<?> createAutocompleteFile(Collection<Map.Entry<String, List<String>>> homepage) {
+        return List.of();
+    }
+
+    @Override
+    public List<String> searchArticles(String queryTerm, Map<?, ?> invertedIndex) {
+        return List.of();
+    }
+}
--- IndexBuilderTest.java
+++ IndexBuilderTest.java
@@ -1,10 +1,71 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-/**
- * @author ericfouh
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class IndexBuilderTest {
-    // ToDo
+    public static final String UPENN_RSS_URL = "https://www.cis.upenn.edu/~cit5940/sample_rss_feed.xml";
+
+    private static String getUpennPageUrl(int pageNum) {
+        return "https://www.seas.upenn.edu/~cit5940/page" + pageNum + ".html";
+    }
+
+    @Test
+    public void testParseFeedWithValidRssUrl() {
+        List<String> feeds = new ArrayList<>();
+        feeds.add(UPENN_RSS_URL);
+
+        IndexBuilder ib = new IndexBuilder();
+        Map<String, List<String>> result = ib.parseFeed(feeds);
+        assertEquals(5, result.size(), "There should have been 5 links parsed");
+
+        for (Map.Entry<String, List<String>> entry : result.entrySet()) {
+            assertTrue(entry.getKey().contains("https://www.seas.upenn.edu/~cit5940/page"));
+            assertFalse(entry.getValue().isEmpty(), "Each web page should have some text");
+        }
+
+        assertEquals(10, result.get(getUpennPageUrl(1)).size());
+
+        List<String> expectedWords = new ArrayList<>(Arrays.asList(
+            "data",
+            "structures",
+            "linear",
+            "data",
+            "structures",
+            "lists",
+            "arraylist",
+            "linkedlist",
+            "stacks",
+            "queues"
+        ));
+        assertEquals(expectedWords, result.get(getUpennPageUrl(1)));
+    }
+
+    @Test
+    public void testParseFeedWithEmptyFeedList() {
+        List<String> feeds = new ArrayList<>();
+        IndexBuilder ib = new IndexBuilder();
+        Map<String, List<String>> result = ib.parseFeed(feeds);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testParseFeedWithInvalidRssUrl() {
+        List<String> feeds = new ArrayList<>();
+        feeds.add("https://does.not.exist/feed.xml");
+        IndexBuilder ib = new IndexBuilder();
+
+        try {
+            Map<String, List<String>> result = ib.parseFeed(feeds);
+            fail("Should have thrown an exception when an invalid rss url is present");
+        } catch (RuntimeException e) {
+            assertTrue(e.getMessage().contains("UnknownHostException"));
+        }
+
+    }
 
 }
